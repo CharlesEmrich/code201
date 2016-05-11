89309635ee3bbb3e4d8f6743f8d689d2ceafaf16
Michael Freeman <mjfreem1@gmail.com>
2016-05-11 14:46:05 -0700

    Fixed Interests

--- wk1/app.js
+++ wk1/app.js
@@ -10,24 +10,21 @@ var ask1 = true;
 var ask2 = true;
 var ask3 = true;
 var ask4 = 0;
-var ask5 = true;
-
-var interests = ['guitar', ' reading', ' foreign languages'];
 
 while (ask1) {
   var userAns = prompt('Was I born in Ukraine?');
   console.log('User\'s response was ' + userAns);
   if (userAns.toUpperCase() === 'Y' || userAns.toLowerCase() === 'yes') {
     alert(userNameF + ', you idiot. That is patently untrue.');
-    console.log(userNameF + ' is a dumbass.');
+    console.log('User\'s response was ' + userAns);
     ask1 = false;
   } else if (userAns.toUpperCase() === 'N' || userAns.toLowerCase() === 'no') {
     alert('Well done, ' + userNameF + '. You\'re not as dumb as you look.');
-    console.log(userNameF + ' passed.');
+    console.log('User\'s response was ' + userAns);
     ask1 = false;
   } else {
     alert('This is a yes/no question, you moron. You have to enter "yes" or "no".');
-    console.log('Pathetic.');
+    console.log('User\'s response was ' + userAns);
   }
 }
 
@@ -36,14 +33,15 @@ while (ask2) {
   console.log('User\'s response was ' + userAns);
   if (userAns.toUpperCase() === 'Y' || userAns.toLowerCase() === 'yes') {
     alert('Are you for real, ' + userNameF + '? Do I look fashionable to you?');
-    console.log('Great job, ' + userNameF + ' You\'re the real MVP.');
+    console.log('User\'s response was ' + userAns);
     ask2 = false;
   } else if (userAns.toUpperCase() === 'N' || userAns.toLowerCase() === 'no') {
     alert('You got it, ' + userNameF + '.');
-    console.log('All hail ' + userNameF + '!');
+    console.log('User\'s response was ' + userAns);
     ask2 = false;
   } else {
     alert('Did you really just try to answer a yes/no question with something besides "yes" or "no", ' + userNameF + ' I can\'t even right now with this.');
+    console.log('User\'s response was ' + userAns);
   }
 }
 
@@ -52,38 +50,52 @@ while (ask3) {
   console.log('User\'s response was ' + userAns);
   if (userAns.toUpperCase() === 'Y' || userAns.toLowerCase() === 'yes') {
     alert('Yes, indeed, ' + userNameF + ', that is correct. You shall be spared.');
-    console.log('\'Tis the end, my only friend. Of our elaborate plan, the end.');
+    console.log('User\'s response was ' + userAns);
     ask3 = false;
   } else if (userAns.toUpperCase() === 'N' || userAns.toLowerCase() === 'no') {
     alert('All you had to do was read the document, ' + userNameF + '.');
-    console.log('I bet you think this song is about you.');
+    console.log('User\'s response was ' + userAns);
     ask3 = false;
   } else {
     alert('Are you kidding me, ' + userNameF + '? It\'s very simple - enter "yes" or "no".');
+    console.log('User\'s response was ' + userAns);
   }
 }
 
-while (ask4 < 3) {
+while (ask4 < 4) {
   var userAns = prompt('If I went graduated from college in 2009, how old am I today?');
   ask4++;
   console.log(userNameF + ' guessed ' + userAns + '.');
-  if (userAns == '29') {
+  if (userAns == 29) {
     alert('Look at John Nash over here...');
-    console.log('A born mathematician.');
+    console.log('User\'s response was ' + userAns);
     { break; }
-  } else if (userAns > 29 || userAns < 29) {
-    alert('Alas, poor ' + userNameF + ' - you knew him not well. Try that shit again.');
-    console.log('A failure? How quaint.');
+  } else if (userAns > 29) {
+    alert('I hope you like the limbo, ' + userNameF + ', because you need to go lower.');
+    console.log('User\'s response was ' + userAns);
+  } else if (userAns < 29) {
+    alert('Call the seminal 90\'s rock band Creed, ' + userNameF + ', because you need to go higher.')
+  } else if (isNaN(userAns)) {
+    alert('A number, doofus. Put in a number.')
+    console.log('User\'s response was ' + userAns);
   }
-  if (ask4 === 3) {
+  if (ask4 === 4) {
     alert('You will never amount to anything, ' + userNameF + '.');
+    console.log('User\'s response was ' + userAns);
   }
 }
 
-while (ask5) {
-  var userAns = prompt('Name an interest of mine.');
-  interests.push(userAns);
-  alert('Yes. My interests include ' + interests + '.');
-  console.log();
-  ask5 = false;
+var interests = ['guitar', 'foreign languages', 'reading', 'traveling'];
+var intAns = prompt('One of my interests is ______.');
+var rightAns = false;
+
+for (var i = 0; i < interests.length; i++) {
+  console.log('interests[' + i + ']: ' + interests[i]);
+  if (intAns === intUp) {
+    alert('Have you been stalking me, ' + userNameF + '? You know me to a T.')
+    rightAns = true;
+  }
+}
+if (!rightAns) {
+  alert('No. Wrong.');
 }
